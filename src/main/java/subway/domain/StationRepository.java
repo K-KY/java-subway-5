c1dff5bf6267893792c6280cb6cc0b3de7befbbe
K-KY <kyuyoungk@naver.com>
2023-12-04 04:04:07 +0900

    Feat(StationRepository) : 기본 등록 역 목록을 스테이션 리포지토리에 저장

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import subway.domain.defaultSetting.DefaultStation;
 
 public class StationRepository {
     private static final List<Station> stations = new ArrayList<>();
@@ -14,6 +15,11 @@ public class StationRepository {
 
     public static void addStation(Station station) {
         stations.add(station);
+    }
+
+    private static void addDefaultStation() {
+        List<DefaultStation> defaultStations = DefaultStation.stations();
+        defaultStations.forEach(defaultStation -> stations.add(new Station(defaultStation.name())));
     }
 
     public static boolean deleteStation(String name) {
