b5ccbc44ed5f92660d67190ef915bbc7b28e820c
K-KY <kyuyoungk@naver.com>
2023-12-05 03:41:35 +0900

    Test(StationRepositoryTest) :  역 추가, 삭제, 읽기 테스트

--- src/test/java/subway/domain/StationRepositoryTest.java
+++ src/test/java/subway/domain/StationRepositoryTest.java
@@ -10,11 +10,28 @@ import subway.domain.defaultSetting.DefaultStation;
 class StationRepositoryTest {
 
     @Test
-    @DisplayName("")
+    @DisplayName("역 목록 가져오기")
     void readStationsTest() {
         List<Station> stations = StationRepository.readStations();
-        stations.forEach(station -> assertThat(StationRepository.isStationExist(station)).isTrue());
+        List<DefaultStation> defaultStations = DefaultStation.stations();
+        defaultStations.forEach(defaultStation -> assertThat(StationRepository.isStationExist(new Station((defaultStation.getStationNames())))).isTrue());
         stations.forEach(station -> System.out.println(station.getName()));
+    }
+
+    @Test
+    @DisplayName("역 추가")
+    void addStationTest() {
+        StationRepository.addStation(new Station("동암역"));
+        assertThat(StationRepository.isStationExist(new Station("동암역"))).isTrue();
+    }
+
+    @Test
+    void deleteStationTest() {
+        StationRepository.addStation(new Station("동암역"));
+        assertThat(StationRepository.isStationExist(new Station("동암역"))).isTrue();
+
+        StationRepository.deleteStation("동암역");
+        assertThat(StationRepository.isStationExist(new Station("동암역"))).isFalse();
     }
 
 }
