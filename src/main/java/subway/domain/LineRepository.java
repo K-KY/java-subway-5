7145bb089f75bfe93a85710f5cef360e977ab394
K-KY <kyuyoungk@naver.com>
2023-12-05 03:35:13 +0900

    Feat(LineRepository) : 기본 노선 목록 생성

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import subway.domain.defaultSetting.DefaultLines;
 
 public class LineRepository {
     private static final List<Line> lines = new ArrayList<>();
 
     public static List<Line> lines() {
+        if (lines.isEmpty()) {
+            addDefaultLine();
+        }
         return Collections.unmodifiableList(lines);
     }
 
@@ -16,7 +20,16 @@ public class LineRepository {
         lines.add(line);
     }
 
+    private static void addDefaultLine() {
+        List<DefaultLines> defaultLines = DefaultLines.lines();
+        defaultLines.forEach(defaultLine -> lines.add(new Line(defaultLine.getLineName())));
+    }
+
     public static boolean deleteLineByName(String name) {
         return lines.removeIf(line -> Objects.equals(line.getName(), name));
+    }
+
+    public static boolean isLineExist(Line line) {
+        return lines.contains(line);
     }
 }
